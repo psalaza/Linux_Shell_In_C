b9aedcf029ddedd4db1c684286fee71334e8a9ca
psalaza <43583947+psalaza@users.noreply.github.com>
2020-01-11 12:40:49 -0500

    Update project.c
    echo arguments work perfectly, also checks for incorrect user input and displays error.

--- project.c
+++ project.c
@@ -12,8 +12,8 @@
 
 typedef struct
 {
-	char** tokens;
-	int numTokens;
+    char** tokens;
+    int numTokens;
 } instruction;
 
 void addToken(instruction* instr_ptr, char* tok);
@@ -21,77 +21,117 @@ void printTokens(instruction* instr_ptr);
 void clearInstruction(instruction* instr_ptr);
 void addNull(instruction* instr_ptr);
 char* expandEnv(const char * name);
+void enviromentalVars(instruction* instr_ptr);
 
 int main() {
-	char* token = NULL;
-	char* temp = NULL;
+    char* token = NULL;
+    char* temp = NULL;
 
-	instruction instr;
-	instr.tokens = NULL;
-	instr.numTokens = 0;
+    instruction instr;
+    instr.tokens = NULL;
+    instr.numTokens = 0;
 
-	while (1) {
+    while (1) {
 
-		printf("%s@%s: %s> ", expandEnv("$USER"), expandEnv("$MACHINE"), expandEnv("$PWD"));
+        printf("%s@%s: %s> ", expandEnv("$USER"), expandEnv("$MACHINE"), expandEnv("$PWD"));
 
-		// loop reads character sequences separated by whitespace
-		do {
-			//scans for next token and allocates token var to size of scanned token
-			scanf("%ms", &token);
-			temp = (char*)malloc((strlen(token) + 1) * sizeof(char));
+        // loop reads character sequences separated by whitespace
+        do {
+            //scans for next token and allocates token var to size of scanned token
+            scanf("%ms", &token);
+            temp = (char*)malloc((strlen(token) + 1) * sizeof(char));
 
-			int i;
-			int start = 0;
-			for (i = 0; i < strlen(token); i++) {
-				//pull out special characters and make them into a separate token in the instruction
-				if (token[i] == '|' || token[i] == '>' || token[i] == '<' || token[i] == '&') {
-					if (i-start > 0) {
-						memcpy(temp, token + start, i - start);
-						temp[i-start] = '\0';
-						addToken(&instr, temp);
-					}
+            int i;
+            int start = 0;
+            for (i = 0; i < strlen(token); i++) {
+                //pull out special characters and make them into a separate token in the instruction
+                if (token[i] == '|' || token[i] == '>' || token[i] == '<' || token[i] == '&') {
+                    if (i-start > 0) {
+                        memcpy(temp, token + start, i - start);
+                        temp[i-start] = '\0';
+                        addToken(&instr, temp);
+                    }
 
-					char specialChar[2];
-					specialChar[0] = token[i];
-					specialChar[1] = '\0';
+                    char specialChar[2];
+                    specialChar[0] = token[i];
+                    specialChar[1] = '\0';
 
-					addToken(&instr,specialChar);
+                    addToken(&instr,specialChar);
 
-					start = i + 1;
-				}
-			}
+                    start = i + 1;
+                }
+            }
 
-			if (start < strlen(token)) {
-				memcpy(temp, token + start, strlen(token) - start);
-				temp[i-start] = '\0';
-				addToken(&instr, temp);
-			}
+            if (start < strlen(token)) {
+                memcpy(temp, token + start, strlen(token) - start);
+                temp[i-start] = '\0';
+                addToken(&instr, temp);
+            }
 
-			//free and reset variables
-			free(token);
-			free(temp);
+            //free and reset variables
+            free(token);
+            free(temp);
 
-			token = NULL;
-			temp = NULL;
-		} while ('\n' != getchar());    //until end of line is reached
+            token = NULL;
+            temp = NULL;
+        } while ('\n' != getchar());    //until end of line is reached
 
-		addNull(&instr);
-		printTokens(&instr);
-		clearInstruction(&instr);
+        enviromentalVars(&instr);
 
+        addNull(&instr);
+       // printTokens(&instr);                 // <----- 	COMMENTED OFF TOKEN PRINTING
+        clearInstruction(&instr);
+
+    }
+
+    return 0;
+}
+
+
+//ECHO BUILT-IN function and user error check when user inputs invalid commands
+void enviromentalVars(instruction* instr_ptr){
+
+    if(strcmp((instr_ptr->tokens)[0],"echo") == 0){
+
+
+        int i;
+        for (i = 1; i < instr_ptr->numTokens; i++) {
+            if(strcmp((instr_ptr->tokens)[i],"$USER") == 0)
+                printf("%s ",getenv("USER"));
+            else if(strcmp((instr_ptr->tokens)[i],"$PATH") == 0)
+                printf("%s ",getenv("PATH"));
+            else if(strcmp((instr_ptr->tokens)[i],"$HOME") == 0)
+                printf("%s ",getenv("HOME"));
+            else if(strcmp((instr_ptr->tokens)[i],"$SHELL") == 0)
+                printf("%s ",getenv("SHELL"));
+            else if(strcmp((instr_ptr->tokens)[i],"$PWD") == 0)
+                printf("%s ",getenv("PWD"));
+            else if((instr_ptr->tokens[i])[0] == '$'){
+                printf("NO SUCH COMMAND EXISTS ");
+            }
+            else
+                printf("%s ", (instr_ptr->tokens[i]));
+        }
+
+    }
+    else{
+		printf("%s: NO SUCH COMMAND FOUND",(instr_ptr->tokens)[0]);
+		
 	}
 
-	return 0;
+    printf("\n");
 }
+
+
 
 // Function to return any proper environmental variable value
 char* expandEnv(const char * name) {
 
-	char subbuff[strlen(name)];
-	memcpy( subbuff, &name[1], strlen(name));
-	subbuff[strlen(name)] = '\0';
-	char * value = getenv(subbuff);
-	return value;
+    char subbuff[strlen(name)];
+    memcpy( subbuff, &name[1], strlen(name));
+    subbuff[strlen(name)] = '\0';
+    char * value = getenv(subbuff);
+    return value;
 
 }
 
@@ -99,49 +139,48 @@ char* expandEnv(const char * name) {
 //allocates for new token within instruction array
 void addToken(instruction* instr_ptr, char* tok)
 {
-	//extend token array to accomodate an additional token
-	if (instr_ptr->numTokens == 0)
-		instr_ptr->tokens = (char**) malloc(sizeof(char*));
-	else
-		instr_ptr->tokens = (char**) realloc(instr_ptr->tokens, (instr_ptr->numTokens+1) * sizeof(char*));
+    //extend token array to accomodate an additional token
+    if (instr_ptr->numTokens == 0)
+        instr_ptr->tokens = (char**) malloc(sizeof(char*));
+    else
+        instr_ptr->tokens = (char**) realloc(instr_ptr->tokens, (instr_ptr->numTokens+1) * sizeof(char*));
 
-	//allocate char array for new token in new slot
-	instr_ptr->tokens[instr_ptr->numTokens] = (char *)malloc((strlen(tok)+1) * sizeof(char));
-	strcpy(instr_ptr->tokens[instr_ptr->numTokens], tok);
+    //allocate char array for new token in new slot
+    instr_ptr->tokens[instr_ptr->numTokens] = (char *)malloc((strlen(tok)+1) * sizeof(char));
+    strcpy(instr_ptr->tokens[instr_ptr->numTokens], tok);
 
-	instr_ptr->numTokens++;
+    instr_ptr->numTokens++;
 }
 
 void addNull(instruction* instr_ptr)
 {
-	//extend token array to accomodate an additional token
-	if (instr_ptr->numTokens == 0)
-		instr_ptr->tokens = (char**)malloc(sizeof(char*));
-	else
-		instr_ptr->tokens = (char**)realloc(instr_ptr->tokens, (instr_ptr->numTokens+1) * sizeof(char*));
+    //extend token array to accomodate an additional token
+    if (instr_ptr->numTokens == 0)
+        instr_ptr->tokens = (char**)malloc(sizeof(char*));
+    else
+        instr_ptr->tokens = (char**)realloc(instr_ptr->tokens, (instr_ptr->numTokens+1) * sizeof(char*));
 
-	instr_ptr->tokens[instr_ptr->numTokens] = (char*) NULL;
-	instr_ptr->numTokens++;
+    instr_ptr->tokens[instr_ptr->numTokens] = (char*) NULL;
+    instr_ptr->numTokens++;
 }
 
 void printTokens(instruction* instr_ptr)
 {
-	int i;
-	printf("Tokens:\n");
-	for (i = 0; i < instr_ptr->numTokens; i++) {
-		if ((instr_ptr->tokens)[i] != NULL)
-			printf("%s\n", (instr_ptr->tokens)[i]);
-	}
+    int i;
+    printf("Tokens:\n");
+    for (i = 0; i < instr_ptr->numTokens; i++) {
+        if ((instr_ptr->tokens)[i] != NULL)
+            printf("%s\n", (instr_ptr->tokens)[i]);
+    }
 }
 
-void clearInstruction(instruction* instr_ptr)
-{
-	int i;
-	for (i = 0; i < instr_ptr->numTokens; i++)
-		free(instr_ptr->tokens[i]);
+void clearInstruction(instruction* instr_ptr) {
+    int i;
+    for (i = 0; i < instr_ptr->numTokens; i++)
+        free(instr_ptr->tokens[i]);
 
-	free(instr_ptr->tokens);
+    free(instr_ptr->tokens);
 
-	instr_ptr->tokens = NULL;
-	instr_ptr->numTokens = 0;
+    instr_ptr->tokens = NULL;
+    instr_ptr->numTokens = 0;
 }
