f1ee40f48a77322c9df8cbe453eae1887279c3d9
Coceus <46695012+Coceus@users.noreply.github.com>
2020-01-28 01:08:15 -0100

    fixes 9

--- project.c
+++ project.c
@@ -178,11 +178,9 @@ int main() {
 			token = NULL;
 			temp = NULL;
 		} while ('\n' != getchar());    //until end of line is reached
-		
 		addNull(&instr);
-		//printTokens(&instr);
 		inputAction(&instr, cmdqueue);
-		//printTokens(&instr);
+		printTokens(&instr);
 		clearInstruction(&instr);
 
 		// Testing to see if queue works
@@ -246,13 +244,13 @@ void inputAction(instruction* instr_ptr, struct Queue* queue) {
 			// Non-Background Process
 			// printf("TOKEN COUNT NO ALLOCATION: %d\n", (sizeof(instr_ptr->tokens)) / (sizeof(char *)));
 			printf("No Allocation:\n");
-			//printTokens(instr_ptr);
+			printTokens(instr_ptr);
 		}
 	}
 
 
 	if (syncheck == 0 || syncheck == 1) {
-		printf("No Allocation1:\n");
+
 		if (syncheck == 1) {
 			printf("Before Allocation:\n");
 			printTokens(instr_ptr);
@@ -271,9 +269,8 @@ void inputAction(instruction* instr_ptr, struct Queue* queue) {
 			printTokens(instr_ptr);
 			//printf("TOKEN COUNT AFTER ALLOCATION: %d\n", (sizeof(instr_ptr->tokens)) / (sizeof(char *)));
 		}
-		
+
 		if (strcmp((instr_ptr->tokens)[0], "echo") == 0) {
-			
 			for (i = 1; i < instr_ptr->numTokens - 1; i++) {
 				if (((instr_ptr->tokens)[i][0]) == '$') {
 					if (expandEnv((instr_ptr->tokens)[i]) == NULL) {
@@ -288,7 +285,6 @@ void inputAction(instruction* instr_ptr, struct Queue* queue) {
 			}
 		}
 		else if (strcmp((instr_ptr->tokens)[0], "cd") == 0) {
-			printf("No Allocation6:\n");
 			if ((instr_ptr->tokens)[2] == NULL) {
 				if ((instr_ptr->tokens)[1] == NULL) {
 					chdir(getenv("HOME"));
@@ -309,7 +305,6 @@ void inputAction(instruction* instr_ptr, struct Queue* queue) {
 		}
 		//pipe command found in user input
 		else if ((instr_ptr->tokens)[1] != NULL && strcmp((instr_ptr->tokens)[1], "|") == 0 || (instr_ptr->tokens)[2] != NULL && strcmp((instr_ptr->tokens)[2], "|") == 0) {
-			printf("No Allocation5:\n");
 			//syntax error check if user does not input 2 arguments with the pipe.
 			if ((instr_ptr->tokens)[2] == NULL) {
 				printf("ERROR: invalid syntax, no 2nd argument found\n");
@@ -319,27 +314,26 @@ void inputAction(instruction* instr_ptr, struct Queue* queue) {
 			}
 		}
 		else {
-			
-			for (i = 0; i < instr_ptr->numTokens; i++) {
-	
-				
-		/*		if (strcmp((instr_ptr->tokens)[i], "..") == 0 ) {
-		
-					
-					
-					instr_ptr->tokens[i] = (char *)realloc((instr_ptr->tokens)[i],(strlen((instr_ptr->tokens)[i]) + 1) * sizeof(char));
+			for (i = 0; i < instr_ptr->numTokens - 1; i++) {
+
+				if (strcmp((instr_ptr->tokens)[i], "..") == 0) {
+
+
+
+					instr_ptr->tokens[i] = (char *)realloc((instr_ptr->tokens)[i], (strlen((instr_ptr->tokens)[i]) + 1) * sizeof(char));
 
 					strcat(instr_ptr->tokens[i], "/");
-				
+
 				}
 				else if (strcmp((instr_ptr->tokens)[i], ".") == 0) {
 
 
-					instr_ptr->tokens[i] = (char *)realloc((instr_ptr->tokens)[i],(strlen((instr_ptr->tokens)[i]) + 1) * sizeof(char));
+					instr_ptr->tokens[i] = (char *)realloc((instr_ptr->tokens)[i], (strlen((instr_ptr->tokens)[i]) + 1) * sizeof(char));
 					strcat(instr_ptr->tokens[i], "/");
-					
-				}*/
+
+				}
 				if ((instr_ptr->tokens)[i] != NULL) {
+
 					check = strrchr(instr_ptr->tokens[i], '/');
 					check2 = strrchr(instr_ptr->tokens[i], '-');
 					check3 = strrchr(instr_ptr->tokens[i], '>');
@@ -503,6 +497,7 @@ void inputAction(instruction* instr_ptr, struct Queue* queue) {
 					}
 					//if (strcmp(recieve, instr_ptr->tokens[i]) == 0) {
 					//}
+
 					if (recieve != NULL) {
 						instr_ptr->tokens[i] = (char *)malloc((strlen(recieve) + 1) * sizeof(char));
 						strcpy(instr_ptr->tokens[i], recieve);
@@ -513,6 +508,7 @@ void inputAction(instruction* instr_ptr, struct Queue* queue) {
 
 			}
 			if (recieve != NULL) {
+
 				my_execute(instr_ptr->tokens, instr_ptr->numTokens, queue, syncheck);
 			}
 			//printf("%s: NO SUCH COMMAND FOUND",(instr_ptr->tokens)[0]);
@@ -537,12 +533,9 @@ char* path(const char * name, int pass) {
 	char *file;
 	char *finisher;
 	char **incompletePath;
-	printf("%d\n", name);
-	printf("%d\n", name);
-	printf("%d\n", name);
-	printf("%d\n", name);
+
 	char *holder = (char*)malloc((strlen(name) + 1) * sizeof(char));
-	printf("%s\n", "11");
+
 	for (i = 0; i < strlen(name) + 1; i++) {
 
 		//pull out special characters and make them into a separate token in the instruction
@@ -573,10 +566,12 @@ char* path(const char * name, int pass) {
 
 				char* pWD = getenv("PWD");
 				ptr = strrchr(pWD, '/');
-
+				printf("%s\n", name);
 				*ptr = '\0';
 				incompletePath[catch22] = (char *)malloc((strlen(pWD) + 2) * sizeof(char));
+				printf("%s\n", name);
 				strcpy(incompletePath[catch22], pWD);
+				printf("%s\n", name);
 				strcat(incompletePath[catch22], "/");
 				*ptr = '/';
 			}
@@ -647,7 +642,9 @@ char* path(const char * name, int pass) {
 
 	}
 
+
 	if (fileExist(finisher) == 1 || pass == 1) {
+
 		return finisher;
 	}
 
@@ -820,7 +817,7 @@ void printTokens(instruction* instr_ptr)
 {
 	int i;
 	printf("Tokens:\n");
-	for (i = 0; i < (instr_ptr->numTokens-1); i++) {
+	for (i = 0; i < (instr_ptr->numTokens); i++) {
 		if ((instr_ptr->tokens)[i] != NULL)
 			printf("%s\n", (instr_ptr->tokens)[i]);
 	}
@@ -828,12 +825,11 @@ void printTokens(instruction* instr_ptr)
 
 void clearInstruction(instruction* instr_ptr) {
 	int i;
-
 	for (i = 0; i < instr_ptr->numTokens; i++)
 		free(instr_ptr->tokens[i]);
 
 	free(instr_ptr->tokens);
-	//printTokens(instr_ptr);
+
 	instr_ptr->tokens = NULL;
 	instr_ptr->numTokens = 0;
 }
